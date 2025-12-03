a6790448f00d6059d7d30c77340156999d9d72e1
Arefe <arefe2003.rezaee@gmail.com>
2025-12-03 15:05:26 +0330

    handleSubmit and post

--- projekt2/src/home/Home.jsx
+++ projekt2/src/home/Home.jsx
@@ -1,29 +1,51 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { FiEdit } from "react-icons/fi";
 import "./home.css";
 import "../assets/style/fonts.css";
+import { IoIosArrowDown } from "react-icons/io";
+
+const data = ["Front-end", "Back-end", "UI/UX", "Mobile", "DevOps"];
 
 const Home = () => {
   const [keyword, setKeyword] = useState("");
   const [result, setResult] = useState(null);
   const [editableText, setEditableText] = useState("");
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+  const ref = useRef(null);
 
   const handleChange = (e) => setKeyword(e.target.value);
 
   const handleSubmit = async () => {
-    if (!keyword) return;
+    if (!keyword || !value) {
+    alert("لطفاً کلیدواژه و حوزه را وارد کنید");
+    return;
+    }
     try {
       const response = await fetch("/api/keywords", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ keyword }),
+        body: JSON.stringify({ keyword , domain: value}),
       });
       const data = await response.json();
       setResult(data);
-      setEditableText(JSON.stringify(data));
+      setEditableText(JSON.stringify());
+      setKeyword("");
+       setValue("");
+    } catch (err) {
       console.error(err);
     }
   };
+
+  useEffect(() => {
+    function handleClickOutside(e) {
+      if (ref.current && !ref.current.contains(e.target)) {
+        setOpen(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
 
   return (
     <div className="container mx-auto justify-center border rounded-[16px] w-[30%] px-8 py-8 mt-[32px] items-start bg-[#f6eafd] border-[#989898]">
@@ -36,11 +58,38 @@ const Home = () => {
           value={keyword}
           onChange={handleChange}
         />
-        <input
-          className="input-home"
-          type="text"
-          placeholder="حوزه مورد نظر را انتخاب کنید"
-        />
+        <div className="w-full relative">
+          <div className="relative">
+           
+            <input
+              className="input-home "
+              type="text"
+              placeholder="حوزه مورد نظر را انتخاب کنید"
+              readOnly
+              value={value}
+              onClick={() => setOpen(!open)}
+            />
+            <IoIosArrowDown className="absolute left-2 top-4.5" />
+          </div>
+
+          {open && (
+            <ul className="absolute left-0 right-0 top-full mt-1 bg-[#faf5fe] border-[#bdbdbd] border rounded-[8px] shadow-2xl z-10">
+              {data.map((item) => (
+                <li
+                  key={item}
+                  className="p-2 hover:bg-[#652583] rounded-[8px] transition-[1s] cursor-pointer"
+                  onClick={() => {
+                    setValue(item);
+                    setOpen(false);
+                  }}
+                >
+                  {item}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+
         <button onClick={handleSubmit} className="button-content">
           شروع تولید محتوا
         </button>
